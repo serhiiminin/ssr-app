4ee986367dcd64f2466a1f9f0b4df0d198548fdf
Serhii Minin <minin.serhii@gmail.com>
2020-08-18 12:39:08 +0300

    feat: add prettier

--- views/App.tsx
+++ views/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
-import reset from 'styled-reset'
-import { IndexPage } from './pages/index';
+import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import { IndexPage } from "./pages/index";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-`
+`;
 
 const S = styled.div`
   background: black;
   color: white;
-
-`
+`;
 
 export const App = () => (
-  <ThemeProvider theme={{ mode: 'light' }}>
-    <GlobalStyle/>
+  <ThemeProvider theme={{ mode: "light" }}>
+    <GlobalStyle />
     <S>hello</S>
-    <div><IndexPage/></div>
+    <div>
+      <IndexPage />
+    </div>
   </ThemeProvider>
 );
--- views/pages/index.tsx
+++ views/pages/index.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import Helmet from 'react-helmet'; 
+import React, { useState } from "react";
+import Helmet from "react-helmet";
 
 export const IndexPage = () => {
   const [count, setCount] = useState<number>(0);
   const onClick = () => {
     setCount((c: number) => c + 1);
-  }
+  };
 
   return (
     <>
       <Helmet>
-          <title>Index Page</title>
-          <meta name="description" content="index page" />
+        <title>Index Page</title>
+        <meta name="description" content="index page" />
       </Helmet>
       <button onClick={onClick}>Save</button>
       <div>Index page {count}</div>
     </>
-  )
+  );
 };
