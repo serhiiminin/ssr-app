3637b4df65a246ea187db5b37860285f4a1e3251
Serhii Minin <minin.serhii@gmail.com>
2020-09-04 23:58:33 +0300

    refactor: remove service worker

--- client/index.tsx
+++ client/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from '../views/App';
 
-import * as serviceWorker from './serviceWorker';
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const data = window.GLOBAL_DATA;
+
 ReactDOM.hydrate(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,8 +15,3 @@ ReactDOM.hydrate(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
